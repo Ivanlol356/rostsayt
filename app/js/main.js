5d3939e440bd4ba8709371eae97fbc80626ed4fe
Ivanlol356 <sivant333@gmail.com>
2020-08-03 14:21:21 +0400

    comments
    осталось 3 не сделанных блока + точки

--- app/js/main.js
+++ app/js/main.js
@@ -1,37 +1,61 @@
 $(function () {
-    $('.slider-inner').slick({
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1000,
-        arrows: true,
-        prevArrow: '<button id="prev" type="button" class="prev"></button>',
-        nextArrow: '<button id="next" type="button" class="next"></button>'
-      });
-    $('.slider-reviews-inner').slick({
-      slidesToShow: 2,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 1000,
-      arrows: true,
-      prevArrow: '<button id="prev" type="button" class="prev"></button>',
-      nextArrow: '<button id="next" type="button" class="next"></button>'
-    });
-    $('.slider-services').slick({
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 1000,
-      arrows: true,
-      prevArrow: '<button id="prev" type="button" class="prev"></button>',
-      nextArrow: '<button id="next" type="button" class="next"></button>'
-    });
-    $('.slider-youtube').slick({
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      arrows: true,
-      prevArrow: '<button id="prev" type="button" class="prev"></button>',
-      nextArrow: '<button id="next" type="button" class="next"></button>'
-    });
-    
+  $('.slider-inner').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: true,
+    prevArrow: '<button id="prev" type="button" class="prev"></button>',
+    nextArrow: '<button id="next" type="button" class="next"></button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+  $('.slider-reviews-inner').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: true,
+    prevArrow: '<button id="prev" type="button" class="prev"></button>',
+    nextArrow: '<button id="next" type="button" class="next"></button>'
+  });
+  $('.slider-services').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: true,
+    prevArrow: '<button id="prev" type="button" class="prev"></button>',
+    nextArrow: '<button id="next" type="button" class="next"></button>'
+  });
+  $('.slider-youtube').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '<button id="prev" type="button" class="prev"></button>',
+    nextArrow: '<button id="next" type="button" class="next"></button>'
+  });
+
 });
