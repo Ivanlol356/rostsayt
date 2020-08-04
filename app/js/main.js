7d15af859c89ca631d6f8bfc6abdc3a8eef0fa81
Ivanlol356 <sivant333@gmail.com>
2020-08-04 11:39:12 +0400

    правка

--- app/js/main.js
+++ app/js/main.js
@@ -7,30 +7,6 @@ $(function () {
     arrows: true,
     prevArrow: '<button id="prev" type="button" class="prev"></button>',
     nextArrow: '<button id="next" type="button" class="next"></button>',
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
   });
   $('.slider-reviews-inner').slick({
     slidesToShow: 2,
@@ -57,5 +33,4 @@ $(function () {
     prevArrow: '<button id="prev" type="button" class="prev"></button>',
     nextArrow: '<button id="next" type="button" class="next"></button>'
   });
-
 });
